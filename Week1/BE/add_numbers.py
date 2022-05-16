f5249770909f690a8f5bb15896a90f59e924c1e4
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-05-16 23:31:09 +0100

    basic fix

--- Week1/BE/add_numbers.py
+++ Week1/BE/add_numbers.py
@@ -1,4 +1,6 @@
+print("The program Add Numbers 2 together. Ensure you pass in a number")
 num1 = int (input("Enter the first Number: "))
 num2 = int (input("Enter the second Number: "))
 sum = num1 + num2
+
 print(f"The sum of {num1} and {num2} is", sum)
