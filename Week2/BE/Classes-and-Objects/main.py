200180a466a1a337c638ed61bb52aa50e04bf9a2
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-05-26 23:32:57 +0100

    complete the OOP task

--- Week2/BE/Classes-and-Objects/main.py
+++ Week2/BE/Classes-and-Objects/main.py
@@ -1,7 +1,30 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    def change_name(self, new_name):
+        self.name = new_name
+        print(self.name)
+        return self.name
+
+    def change_age(self, new_age):
+        self.age = new_age
+        print(self.age)
+        return self.age
+
+  
+    def add_track(self, track):
+        self.tracks.append(track)
+        print(self.tracks)
+        return self.tracks
+
+    def get_score(self):
+        print(self.score)
+        return self.score
 
 
 
